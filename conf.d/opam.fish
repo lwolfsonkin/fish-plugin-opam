de53296b26e84b2b7265a3d202f89eee676fef03
Lawrence <lawrencews@jhu.edu>
2018-02-12 14:45:38 -0500

    Added more compatible manpath extension

--- conf.d/opam.fish
+++ conf.d/opam.fish
@@ -3,7 +3,7 @@
 if command -s opam > /dev/null
     set -gx CAML_LD_LIBRARY_PATH "$HOME/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
     set -gx OPAMUTF8MSGS "1";
-    set -gx MANPATH "$HOME/.opam/system/man" $MANPATH;
+    set -gx MANPATH "$HOME/.opam/system/man":(manpath);
     set -gx PERL5LIB "$HOME/.opam/system/lib/perl5";
     set -gx OCAML_TOPLEVEL_PATH "$HOME/.opam/system/lib/toplevel";
     set -gx PATH "$HOME/.opam/system/bin" $PATH;
