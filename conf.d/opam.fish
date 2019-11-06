86f83d413f6f82d13cb6552cd614f7767860996c
Vadzim Tonka <vadzim@tonka.space>
2019-11-06 14:22:01 +0100

    Make plugin use current Opam switch instead of system one

--- conf.d/opam.fish
+++ conf.d/opam.fish
@@ -1,12 +1,13 @@
 # set the user installation path
 
 if command -s opam > /dev/null
-    set -gx CAML_LD_LIBRARY_PATH "$HOME/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs";
+    set -l switch (opam switch show)
+    set -gx CAML_LD_LIBRARY_PATH "$HOME/.opam/$switch/lib/stublibs:/usr/local/lib/ocaml/stublibs";
     set -gx OPAMUTF8MSGS "1";
-    set -gx MANPATH "$HOME/.opam/system/man":(manpath);
-    set -gx PERL5LIB "$HOME/.opam/system/lib/perl5";
-    set -gx OCAML_TOPLEVEL_PATH "$HOME/.opam/system/lib/toplevel";
-    set -gx PATH "$HOME/.opam/system/bin" $PATH;
+    set -gx MANPATH "$HOME/.opam/$switch/man":(manpath);
+    set -gx PERL5LIB "$HOME/.opam/$switch/lib/perl5";
+    set -gx OCAML_TOPLEVEL_PATH "$HOME/.opam/$switch/lib/toplevel";
+    set -gx PATH "$HOME/.opam/$switch/bin" $PATH;
 else
     function opam -d "https://opam.ocaml.org/doc/Install.html"
         echo "Install https://opam.ocaml.org/doc/Install.html to use this plugin." > /dev/stderr
